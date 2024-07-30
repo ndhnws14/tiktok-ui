89f09617597075c49e723080aec54eb99c30da91
ndhnws14 <donghonew14@gmail.com>
2024-07-30 14:51:29 +0700

    tai cau truc code va sd PropTypes vao project

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
+import config from '~/config';
+
 //Layouts
-import { HeaderOnly } from '~/component/Layout';
+import { HeaderOnly } from '~/layouts';
 
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
@@ -9,11 +11,11 @@ import Search from '~/pages/Search';
 
 //public routes
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/@:nickname', component: Profile },
-    { path: '/search', component: Search, layout: null },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.search, component: Search, layout: null },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
 ];
 //private routes
 const privateRoutes = [];
