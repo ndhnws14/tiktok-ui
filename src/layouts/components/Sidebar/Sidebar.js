1ec9192b2994f12f02338c9700405f263c20c1ca
ndhnws14 <donghonew14@gmail.com>
2024-07-31 00:29:40 +0700

    xay dung ui Sidebar

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,33 @@
+import classNames from 'classnames/bind';
+import Menu, { MenuItem } from './Menu';
+import styles from './Sidebar.module.scss';
+import config from '~/config';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+} from '~/components/Icons';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="Live" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,6 +8,7 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 //public routes
 const publicRoutes = [
@@ -16,6 +17,7 @@ const publicRoutes = [
     { path: config.routes.profile, component: Profile },
     { path: config.routes.search, component: Search, layout: null },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.live, component: Live },
 ];
 //private routes
 const privateRoutes = [];
